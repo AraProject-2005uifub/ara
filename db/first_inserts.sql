c99c9de3d2a0a8e4432ec0aff4344d8d930bacf1
Myyyth <bas9339@gmail.com>
2017-03-31 01:45:57 +0300

    database

---
+++ db/first_inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO kinds_of_users (kind)
+VALUES ("Administrator");
+
+INSERT INTO kinds_of_users (kind)
+VALUES ("Head of Unit");
+
+INSERT INTO kinds_of_users (kind)
+VALUES ("University Administrator");
